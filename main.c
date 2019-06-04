66b8ef732451b5428166c08b98e1d644710fab81
wangyang <wangyang.xjtu@foxmail.com>
2019-06-04 14:08:35 +0800

    简化后的截屏程序

--- main.c
+++ main.c
@@ -4,65 +4,24 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include "memory_pool.h"
 #include "capture.h"
 #include "jpeg_util.h"
 
 int main(int argc,char *argv[])
 {
-#ifdef DECODE_TEST
-	if(argc != 3)
-	{
-		printf("usage: ./capture_screen [Source JPEG File Path] [JPEG File Path To Save]\n");
-		return -1;
-	}
-
-	memory_pool_t *pool = memory_pool_create(20*1024*1024);
-
-	uint32_t width = 0,height = 0;
-	uint8_t *outbuf = NULL;
-	uint64_t outlen = 0;
-	uint32_t bpp;
-
-	uint8_t *rgb24 = (uint8_t *)memory_pool_alloc(10*1024*1024);
-	if(decode_jpeg(argv[1],&width,&height,&bpp,rgb24) < 0)
-	{
-		fprintf(stderr,"%s\n","decode jpeg file failed!");
-		return -1;
-	}
-	printf("========JPEG File Informatoin =========\n");
-	printf("Image size: %d * %d * %d = %d\n",width,height,bpp,width*height*bpp);
-	printf("=======================================\n");
-
-	encode_jpeg(rgb24,width,height,&outbuf,&outlen);
-
-	int fd = open(argv[2],O_WRONLY|O_CREAT,0664);
-	if(write(fd,outbuf,outlen) != (ssize_t)outlen)
-	{
-		printf("write failed: %s\n",strerror(errno));
-		return 0;
-	}
-	close(fd);
-	printf("Image length: %d\n",outlen);
-	printf("Screen capture has been saved into %s\n",argv[2]);
-	memory_pool_destroy(pool);
-#endif
-#ifdef ENCODE_TEST
 	if(argc != 2)
 	{
 		printf("usage: ./capture_screen [JPEG File Path To Save]\n");
 		return -1;
 	}
 
-	memory_pool_t *global_pool = memory_pool_create(20*1024*1024);
 	uint32_t width = 0,height = 0;
 	uint8_t *outbuf = NULL;
 	uint64_t outlen = 0;
 	get_window_size(&width,&height);
-	uint8_t *rgb24 = (uint8_t *)memory_pool_alloc(global_pool,width*height*3);
+	uint8_t *rgb24 = (uint8_t *)malloc(width*height*3);
 	CaptureDesktop(rgb24);  
 	encode_jpeg(rgb24,width,height,&outbuf,&outlen);
-
 	CaptureDesktop(rgb24);  
 	encode_jpeg(rgb24,width,height,&outbuf,&outlen);
 
@@ -75,10 +34,8 @@ int main(int argc,char *argv[])
 	close(fd);
 	printf("Image size: %d * %d\n",width,height);
 	printf("Image length: %ld\n",outlen);
-
 	printf("Screen capture has been saved into %s\n",argv[1]);
-	memory_pool_destroy(global_pool);
-#endif
+	free(rgb24);
 	return 0;
 }
 
